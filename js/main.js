c2c10af6a3df1f0cab247042be6aa3222645ed5b
IlliaTemnov <ilyatemnov@gmail.com>
2020-06-09 16:36:50 +0300

    Popup added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var map = document.querySelector('.map');
-var template = document.querySelector('#pin');
+var templatePin = document.querySelector('#pin');
 var mapPins = document.querySelector('.map__pins');
 
 var UNITS_NUMBER = 8;
@@ -83,7 +83,7 @@ var createUnitsArr = function (unitsQuantity) {
 map.classList.remove('map--faded');
 
 var getRenderUnit = function (unitObj) {
-  var renderPinTemplate = template.cloneNode(true);
+  var renderPinTemplate = templatePin.cloneNode(true);
   var pinButton = renderPinTemplate.content.querySelector('.map__pin');
   var avatarImg = renderPinTemplate.content.querySelector('img');
 
@@ -96,10 +96,16 @@ var getRenderUnit = function (unitObj) {
 };
 
 var fragment = document.createDocumentFragment();
-// for (var i = 0; i < UNITS_NUMBER; i++) {
-//   fragment.appendChild(getRenderUnit(createUnitsArr(UNITS_NUMBER)));
-// }
+
 var ads = createUnitsArr(UNITS_NUMBER);
+
+// ____________________________________
+
+// for (var i = 0; i < ads.length; i++) {
+//   var item = getRenderUnit(ads[i]);
+//   fragment.appendChild(item.content);
+// }
+// ____________________________
 
 ads.forEach(function (ad) {
   var item = getRenderUnit(ad);
@@ -107,3 +113,78 @@ ads.forEach(function (ad) {
 });
 
 mapPins.appendChild(fragment);
+
+// ___________________________________________________________________________________
+
+var fillAdsCard = function (arr, template) {
+
+  var popupAdsTitle = template.querySelector('.popup__title');
+  var popupAdsAdress = template.querySelector('.popup__text--address');
+  var popupAdsPrice = template.querySelector('.popup__text--price');
+  var popupAdsType = template.querySelector('.popup__type');
+  var popupAdsCapacity = template.querySelector('.popup__text--capacity');
+  var popupAdsTime = template.querySelector('.popup__text--time');
+  var popupFeatures = template.querySelector('.popup__features');
+  var popupDescription = template.querySelector('.popup__description');
+  var popupPhotos = template.querySelector('.popup__photos');
+  var popupAvatar = template.querySelector('.popup__avatar');
+
+  var checkHousingType = function () {
+    if (arr[0].offer.type === 'flat') {
+      return 'Квартира';
+    } else if (arr[0].offer.type === 'bungalo') {
+      return 'Бунгало';
+    } else if (arr[0].offer.type === 'house') {
+      return 'Дом';
+    } else {
+      return 'Дворец';
+    }
+  };
+
+  if (arr[0].offer.hasOwnProperty('title')) {
+    popupAdsTitle.textContent = arr[0].offer.title;
+  } else {
+    popupAdsTitle.remove();
+  } if (arr[0].offer.hasOwnProperty('adress')) {
+    popupAdsAdress.textContent = arr[0].offer.adress;
+  } else {
+    popupAdsAdress.remove();
+  } if (arr[0].offer.hasOwnProperty('price')) {
+    popupAdsPrice.textContent = arr[0].offer.price + ' ₽/ночь';
+  } else {
+    popupAdsPrice.remove();
+  } if (arr[0].offer.hasOwnProperty('type')) {
+    popupAdsType.textContent = checkHousingType(arr[0].offer.type);
+  } else {
+    popupAdsType.remove();
+  } if (arr[0].offer.hasOwnProperty('rooms') && arr[0].offer.hasOwnProperty('guests')) {
+    popupAdsCapacity.textContent = arr[0].offer.rooms + ' комнаты';
+  } else {
+    popupAdsCapacity.remove();
+  } if (arr[0].offer.hasOwnProperty('checkin') && arr[0].offer.hasOwnProperty('checkout')) {
+    popupAdsTime.textContent = 'Заезд после' + arr[0].offer.checkin + ', выезд до' + arr[0].offer.checkout;
+  } else {
+    popupAdsTime.remove();
+  } if (arr[0].offer.hasOwnProperty('features')) {
+    popupFeatures.textContent = arr[0].offer.features;
+  } else {
+    popupFeatures.remove();
+  } if (arr[0].offer.hasOwnProperty('photos')) {
+    popupPhotos.src = arr[0].offer.photos;
+  } else {
+    popupPhotos.remove();
+  } if (arr[0].author.hasOwnProperty('avatar')) {
+    popupAvatar.src = arr[0].author.avatar;
+  } else {
+    popupAvatar.remove();
+  }
+};
+
+var adsCardTemplate = document.querySelector('#card').content;
+var adsCard = adsCardTemplate.querySelector('.map__card').cloneNode(true);
+var randomCardObject = ads;
+
+fillAdsCard(randomCardObject, adsCard);
+
+var mapPinsAds = document.querySelector('.map .map__pins');
+mapPinsAds.insertAdjacentElement('afterend', adsCard);
